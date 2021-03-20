91bb44d2fbabe98366ff58c0dcd90f1694dbae1f
Vitalii Isaenko <lookyisaenko@gmail.com>
2021-03-20 17:34:41 +0100

    Change pictures to be a list element

--- js/index.js
+++ js/index.js
@@ -1,16 +1,29 @@
 let profile = document.querySelector('.profile');
-let editButton = profile.querySelector('.profile__edit');
-
 let popup = document.querySelector('.popup');
+
 let form = popup.querySelector('.popup__popup-container');
-
-let popupCloseButton = form.querySelector('.popup__close');
-
 form.addEventListener('submit', formSubmitHandler);
 
+let editButton = profile.querySelector('.profile__edit');
 editButton.addEventListener('click', openPopup);
+
+let popupCloseButton = form.querySelector('.popup__close');
 popupCloseButton.addEventListener('click', closePopup);
 
+function formSubmitHandler(evt) {
+    evt.preventDefault();
+
+    let nameInput = form.querySelector('.popup__input-name');
+    let jobInput = form.querySelector('.popup__input-job');
+
+    let profileName = profile.querySelector('.profile__name');
+    let profileJob = profile.querySelector('.profile__job');
+
+    profileName.textContent = nameInput.value;
+    profileJob.textContent = jobInput.value;
+
+    closePopup();
+}
 
 function openPopup() {
     let inputName = form.querySelector('.popup__input-name');
@@ -26,19 +39,4 @@ function openPopup() {
 
 function closePopup() {
     popup.classList.remove('popup_opened');
-}
-
-function formSubmitHandler(evt) {
-    evt.preventDefault();
-
-    let nameInput = form.querySelector('.popup__input-name');
-    let jobInput = form.querySelector('.popup__input-job');
-
-    let profileName = profile.querySelector('.profile__name');
-    let profileJob = profile.querySelector('.profile__job');
-
-    profileName.textContent = nameInput.value;
-    profileJob.textContent = jobInput.value;
-
-    closePopup();
 }
