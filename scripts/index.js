8a737be6383f7322cf9a369ed65ce17fed13e78b
Vitalii Isaenko <lookyisaenko@gmail.com>
2021-04-03 16:16:48 +0200

    Add initial cards, initialize them in list

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,26 @@
-let profile = document.querySelector('.profile');
-let editButton = profile.querySelector('.profile__edit');
-let profileName = profile.querySelector('.profile__name');
-let profileJob = profile.querySelector('.profile__job');
+const profile = document.querySelector('.profile');
+const editButton = profile.querySelector('.profile__edit');
+const profileName = profile.querySelector('.profile__name');
+const profileJob = profile.querySelector('.profile__job');
 
-let popup = document.querySelector('.popup');
-let form = popup.querySelector('.form');
+const popup = document.querySelector('.popup');
+const form = popup.querySelector('.form');
 
-let popupCloseButton = popup.querySelector('.popup__close');
-let nameInput = form.querySelector('.form__input_name_name');
-let jobInput = form.querySelector('.form__input_name_job');
+const popupCloseButton = popup.querySelector('.popup__close');
+const nameInput = form.querySelector('.form__input_name_name');
+const jobInput = form.querySelector('.form__input_name_job');
 
+const pictureTemplate = document.querySelector('#picture-template').content;
+const pictureList = document.querySelector('.pictures__list');
+
+function createCard(name, photoLink) {
+    const pictureCopy = pictureTemplate.cloneNode(true);
+    const picturePhoto = pictureCopy.querySelector('.pictures__item-photo');
+    const pictureName = pictureCopy.querySelector('.pictures__item-name');
+    picturePhoto.src = photoLink;
+    pictureName.textContent = name;
+    return pictureCopy;
+}
 
 function formSubmitHandler(evt) {
     evt.preventDefault();
@@ -30,6 +41,42 @@ function closePopup() {
     popup.classList.remove('popup_opened');
 }
 
+function initializeCards() {
+    const cards = [];
+    initialCards.forEach(el => {
+        cards.push(createCard(el.name, el.link));
+    });
+    pictureList.append(...cards);
+}
+
+const initialCards = [{
+        name: 'Архыз',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+        name: 'Челябинская область',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+        name: 'Иваново',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+        name: 'Камчатка',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+        name: 'Холмогорский район',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+];
+
 form.addEventListener('submit', formSubmitHandler);
 editButton.addEventListener('click', openPopup);
 popupCloseButton.addEventListener('click', closePopup);
+
+initializeCards();
