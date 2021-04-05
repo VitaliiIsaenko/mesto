3cf041050e4b37e2ad2f8eba516141f076c88f0c
Vitalii Isaenko <lookyisaenko@gmail.com>
2021-04-05 13:02:30 +0200

    Cleanup

--- scripts/index.js
+++ scripts/index.js
@@ -24,6 +24,7 @@ const initialCards = [{
     }
 ];
 
+// Profile
 const profile = document.querySelector('.profile');
 const profileEditButton = profile.querySelector('.profile__edit');
 const profileName = profile.querySelector('.profile__name');
@@ -36,22 +37,77 @@ const profileCloseButton = profilePopup.querySelector('.popup__close');
 const profileNameInput = profileForm.querySelector('.form__input_type_name');
 const profileJobInput = profileForm.querySelector('.form__input_type_job');
 
+// Pictures
+const picturePopup = document.querySelector('.popup_type_picture');
+const pictureCloseButton = picturePopup.querySelector('.popup__close');
+
+const pictureList = document.querySelector('.pictures__list');
+
+// Cards
 const cardAddButton = profile.querySelector('.profile__add');
 
 const cardPopup = document.querySelector('.popup_type_add-card');
 const cardForm = cardPopup.querySelector('.form');
 
 const cardCloseButton = cardPopup.querySelector('.popup__close');
-const cardNameInput = cardForm.querySelector('.form__input_type_name');
-const cardPictureLinkInput = cardForm.querySelector('.form__input_type_picture-link');
 
-const picturePopup = document.querySelector('.popup_type_picture');
-const pictureCloseButton = picturePopup.querySelector('.popup__close');
 
-const pictureTemplate = document.querySelector('#picture-template').content;
-const pictureList = document.querySelector('.pictures__list');
+
+function openPopup(popup) {
+    popup.classList.add('popup_opened');
+}
+
+function closePopup(popup) {
+    popup.classList.remove('popup_opened');
+}
+
+// Profile
+function openProfilePopup(profileName, profileJob) {
+    profileNameInput.value = profileName;
+    profileJobInput.value = profileJob;
+    openPopup(profilePopup);
+}
+
+function profileFormSubmitHandler(evt) {
+    evt.preventDefault();
+
+    profileName.textContent = profileNameInput.value;
+    profileJob.textContent = profileJobInput.value;
+
+    closePopup(profilePopup);
+}
+
+// Pictures
+function openPicturePopup(name, link) {
+    const popupCaption = picturePopup.querySelector('.popup__image-caption');
+    popupCaption.textContent = name;
+    const popupImage = picturePopup.querySelector('.popup__image');
+    popupImage.src = link;
+    popupImage.alt = name;
+
+    openPopup(picturePopup);
+}
+
+// Cards
+function cardFormSubmitHandler(evt) {
+    evt.preventDefault();
+
+    const cardNameInput = cardForm.querySelector('.form__input_type_name');
+    const cardPictureLinkInput = cardForm.querySelector('.form__input_type_picture-link');
+
+    const card = createCard(cardNameInput.value, cardPictureLinkInput.value);
+    pictureList.prepend(card);
+
+    closeCardPopup();
+}
+
+function closeCardPopup() {
+    cardForm.reset();
+    closePopup(cardPopup);
+}
 
 function createCard(name, photoLink) {
+    const pictureTemplate = document.querySelector('#picture-template').content;
     const pictureCopy = pictureTemplate.cloneNode(true);
     const picturePhoto = pictureCopy.querySelector('.pictures__item-photo');
     const pictureName = pictureCopy.querySelector('.pictures__item-name');
@@ -76,69 +132,25 @@ function createCard(name, photoLink) {
     return pictureCopy;
 }
 
-function profileFormSubmitHandler(evt) {
-    evt.preventDefault();
-
-    profileName.textContent = profileNameInput.value;
-    profileJob.textContent = profileJobInput.value;
-
-    closePopup(profilePopup);
-}
-
-function openProfilePopup(profileName, profileJob) {
-    profileNameInput.value = profileName;
-    profileJobInput.value = profileJob;
-    openPopup(profilePopup);
-}
-
-function openPopup(popup) {
-    popup.classList.add('popup_opened');
-}
-
-function cardFormSubmitHandler(evt) {
-    evt.preventDefault();
-
-    const card = createCard(cardNameInput.value, cardPictureLinkInput.value);
-    pictureList.prepend(card);
-
-    closeCardPopup();
-}
-
-function closeCardPopup() {
-    cardForm.reset();
-    closePopup(cardPopup);
-}
-
-function openPicturePopup(name, link) {
-    const popupCaption = picturePopup.querySelector('.popup__image-caption');
-    popupCaption.textContent = name;
-    const popupImage = picturePopup.querySelector('.popup__image');
-    popupImage.src = link;
-    popupImage.alt = name;
-
-    openPopup(picturePopup);
-}
-
-function closePopup(popup) {
-    popup.classList.remove('popup_opened');
-}
-
 function initializeCards() {
-    // почему-то я думал, что вариант с созданием массива и использованием append() на
-    // массиве элементов будет эффективнее, так как перерисовка будет происходить 1 раз, а не каждый раз в цикле
+    // я думал, что вариант с созданием массива и использованием append() на массиве элементов будет эффективнее, 
+    // так как перерисовка будет происходить 1 раз, а не каждый раз в цикле
     initialCards.forEach(el => {
         pictureList.append(createCard(el.name, el.link));
     });
 }
 
+// Profile
 profileForm.addEventListener('submit', profileFormSubmitHandler);
 profileEditButton.addEventListener('click', evt => openProfilePopup(profileName.textContent, profileJob.textContent));
 profileCloseButton.addEventListener('click', _ => closePopup(profilePopup));
 
+// Pictures
+pictureCloseButton.addEventListener('click', _ => closePopup(picturePopup));
+
+// Cards
 cardForm.addEventListener('submit', cardFormSubmitHandler);
 cardAddButton.addEventListener('click', _ => openPopup(cardPopup));
 cardCloseButton.addEventListener('click', closeCardPopup);
-
-pictureCloseButton.addEventListener('click', _ => closePopup(picturePopup));
 
 initializeCards();
